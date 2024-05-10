7eac545d5e8b504303a7b51f769e74d69ea0f43e
qmzlr <drozdovvalerij61@gmail.com>
2024-05-11 01:14:44 +0400

    css

--- register.php
+++ register.php
@@ -15,19 +15,19 @@ session_start();
 <form action="vendor/signup.php" method="post" enctype="multipart/form-data">
 
     <label>ФИО</label>
-    <input type="text" name="full_name" placeholder="">
+    <input type="text" name="full_name" placeholder="Введите ФИО">
 
     <label>Логин</label>
     <input type="text" name="login" placeholder="Введите логин">
 
     <label>Email</label>
-    <input type="text" name="email" placeholder="">
+    <input type="text" name="email" placeholder="Введите email">
 
     <label>Пароль</label>
     <input type="password" name="password" placeholder="Введите пароль">
 
     <label>Подтверждение пароля</label>
-    <input type="password" name="password_confirm" placeholder="">
+    <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
 
     <label>Изображение пользователя</label>
     <input type="file" name="avatar">

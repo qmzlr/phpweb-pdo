91c42a36ccb49da181e70bd0dec090cc997a562c
qmzlr <drozdovvalerij61@gmail.com>
2024-05-11 10:57:46 +0400

    11.05 авторизация

--- profile.php
+++ profile.php
@@ -1,1 +1,26 @@
 <?php
+session_start();
+if (!isset($_SESSION['user'])){
+   header('location: ../index.php');
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport"
+         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Профиль</title>
+</head>
+<body>
+   <div class="profile">
+      <h2>Профиль пользователя</h2>
+      <img src="<?=$_SESSION['user']['avatar'] ?>" alt="Аватар пользователя" class="avatar">
+      <p>Ваш Email: <?php echo $_SESSION['user']['email'];?></p>
+      <p>Ваше ФИО: <?php echo $_SESSION['user']['full_name'];?></p>
+      <a href="vendor/logout.php">Выход</a>
+   </div>
+</body>
+</html>
--- vendor/signin.php
+++ vendor/signin.php
@@ -8,7 +8,15 @@ $password = md5($_POST['password']);
 $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
 
 if (mysqli_num_rows($check_user) > 0) {
+   $user = mysqli_fetch_assoc($check_user);
+   $_SESSION['user'] = [
+      'id' => $user['id'],
+      'full_name' => $user['full_name'],
+      'avatar' => $user['avatar'],
+      'email' => $user['email'],
+   ];
+   header('Location: ../profile.php');
 } else {
    $_SESSION['message'] = 'Неверный логин или пароль';
-   header('location: ../index.php');
+   header('Location: ../index.php');
 }
